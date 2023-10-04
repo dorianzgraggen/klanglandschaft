c8d4a0389900ce0629f289331d445c1bf78550b8
Dorian Zgraggen <dorianfx1@gmail.com>
2023-10-04 23:13:04 +0200

    unzip

--- datagen/app.ts
+++ datagen/app.ts
@@ -1,14 +1,17 @@
-import * as fs from "fs";
-import * as https from "https";
+import fs from "fs";
+import https from "https";
 import path from "path";
+import AdmZip from "adm-zip";
 
 run();
 
 async function run() {
-  download_if_missing(
+  await download_if_missing(
     "xyz.zip",
     "https://data.geo.admin.ch/ch.swisstopo.swisssurface3d-raster/swisssurface3d-raster_2020_2666-1211/swisssurface3d-raster_2020_2666-1211_0.5_2056_5728.xyz.zip"
   );
+
+  unzip("xyz.zip", { delete_source: false });
 }
 
 async function download_if_missing(filepath: string, url: string) {
@@ -31,4 +34,37 @@ async function download_if_missing(filepath: string, url: string) {
       });
     });
   });
+}
+
+function unzip(filepath: string, args?: { delete_source: boolean }) {
+  const _args = {
+    delete_source: true,
+    ...args,
+  };
+
+  const zip = new AdmZip(pathify(filepath));
+  const entries = zip.getEntries();
+
+  if (entries.length !== 1) {
+    throw new Error("Expected zip to have one file only");
+  }
+
+  if (fs.existsSync(pathify(entries[0].entryName))) {
+    console.log("Skipped unzipping, already exists");
+    return;
+  }
+
+  try {
+    zip.extractEntryTo(entries[0], pathify("/"));
+  } catch (error) {
+    console.error("Couldn't unzip:", error);
+  }
+
+  if (_args.delete_source) {
+    fs.unlinkSync(pathify("xyz.zip"));
+  }
+}
+
+function pathify(file_path: string) {
+  return path.join("files", file_path);
 }
