1a50577114b8ce350ff0cc777fbc964eeafb5b73
Dorian Zgraggen <dorianfx1@gmail.com>
2023-10-18 14:18:40 +0200

    refactor: delete consts.ts

--- frontend/src/sound_editor/consts.ts
+++
@@ -1,3 +0,0 @@
-export const botAvatar = 'https://robohash.org/liberovelitdolores.bmp?size=50x50&set=set1';
-export const userAvatar =
-  'http://icons.iconarchive.com/icons/visualpharm/must-have/256/User-icon.png';
