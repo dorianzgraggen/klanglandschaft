16b206c370190adf15f27660ee457fbad5b9204a
Dorian Zgraggen <dorianfx1@gmail.com>
2023-11-08 11:46:19 +0100

    refactor: tidy up landscape fragment shader

--- frontend/src/3d/landscape.frag.glsl
+++ frontend/src/3d/landscape.frag.glsl
@@ -4,7 +4,6 @@
 
 uniform sampler2D u_height;
 uniform sampler2D u_satellite;
-uniform sampler2D u_nrm;
 uniform vec3 u_center;
 uniform vec3 u_background;
 
@@ -26,26 +25,10 @@ void main()
   vec3 height = texture2D(u_height, v_uv).xyz;
   vec4 satellite = texture2D(u_satellite, v_uv);
 
-  float dist = distance(v_world_pos.xz, u_center.xz);
-
-  float inner_width = 16.0;
-  float gradient_width = 3.0;
-
-  float mask = (dist - inner_width) / gradient_width;
-  mask = clamp(mask, 0.0, 1.0);
-
-  vec4 color = mix(satellite, fromLinear(vec4(u_background, 1.0)), mask);
-
   float fog_mask = (distance(v_world_pos, cameraPosition) - 30.0) * 0.01;
   fog_mask = clamp(fog_mask, 0.0, 1.0);
 
   vec4 color_fog = mix(satellite, fromLinear(vec4(u_background, 1.0)), fog_mask);
 
-  gl_FragColor = vec4(v_world_pos, 1.0);
-  gl_FragColor = vec4(vec3(mask), 1.0);
-  gl_FragColor = vec4(height, 1.0);
-  gl_FragColor = color;
-  gl_FragColor = vec4(vec3(fog_mask), 1.0);
-  gl_FragColor = satellite;
   gl_FragColor = color_fog;
 }   
