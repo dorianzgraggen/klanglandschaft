d50099beaebe760102dd94b104be4d1b01652160
mmelezanovic <54948369+mmelezanovic@users.noreply.github.com>
2023-12-07 20:55:46 +0100

    chore: add VueUse and refactor mapComp location blob drag feat
    replace mouse events with vueuse useDraggable element to  simplify location blob dragging and constraining it to the visible map areas, as well as code cleanup

--- frontend/src/components/MapComp.vue
+++ frontend/src/components/MapComp.vue
@@ -1,57 +1,54 @@
 <script setup lang="ts">
-import { reactive, ref, watch } from 'vue';
+import { onMounted, ref } from 'vue';
+import { useDraggable } from '@vueuse/core';
 
-const locationBlob = reactive({
+const locationBlobElement = ref<HTMLElement | null>(null);
+const interactiveMapElement = ref<HTMLElement | null>(null);
+
+const locationBlob = {
     size: 20,
-    top: 55,
-    left: 15,
-    gap: 6
+};
+
+onMounted(() => {
+    locationBlobElement.value = document.getElementById('location-blob');
+    interactiveMapElement.value = document.getElementById('map-container');
 });
 
-const mouseHold = ref(false);
+// `style` will be a helper computed for `left: ?px; top: ?px;`
+const { x, y, style } = useDraggable(locationBlobElement, {
+    containerElement: interactiveMapElement,
+})
 
-
-function updateBlobLocation(event: MouseEvent) {
-    const mapContainer = document.querySelector('.map-container');
-    if (mapContainer) {
-        const rect = mapContainer.getBoundingClientRect();
-        const x = event.clientX - rect.left - locationBlob.size / 2;
-        const y = event.clientY - rect.top + locationBlob.size;
-        if (x < 8 || x > rect.width - locationBlob.size - locationBlob.gap || y < 40 || y > 183) {
-            return;
-        }
-        locationBlob.top = y;
-        locationBlob.left = x;
-    }
-}
-
-function toggleMouseHold() {
-    mouseHold.value = !mouseHold.value;
-}
-
-watch(mouseHold, (newValue) => {
-    if (newValue) {
-        document.addEventListener('mousemove', updateBlobLocation);
-    } else {
-        document.removeEventListener('mousemove', updateBlobLocation);
-    }
-});
+//TODO: send out location blob's position to backend
 
 </script>
 
 <template>
     <div class="component-title">map</div>
-    <div @mousedown.prevent='toggleMouseHold' @mouseup.prevent='toggleMouseHold' @click='updateBlobLocation'
-        class="map-container border-corners">
-        <img src="../assets/outline_vierwaldstaettersee.png" alt="map" />
-        <button id="location-blob"></button>
+    <div id="map-container" class="border-corners">
+        <div id="interactive-area" ref="interactiveMapElement">
+            <img draggable=false src="../assets/outline_vierwaldstaettersee.png" alt="map" />
+            <button id="location-blob" ref="locationBlobElement" :style="style"></button>
+        </div>
+
+
     </div>
 </template>
 
 <style scoped>
-.map-container {
+#map-container {
+    position: relative;
+    display: flex;
+    justify-content: center;
+    align-items: center;
     width: 200px;
     height: 170px;
+}
+
+#interactive-area {
+    position: relative;
+    width: 98%;
+    height: 98%;
 }
 
 img {
@@ -63,8 +60,6 @@ img {
     height: v-bind('locationBlob.size + "px"');
     width: v-bind('locationBlob.size + "px"');
     padding: 0;
-    top: v-bind('locationBlob.top + "px"');
-    left: v-bind('locationBlob.left + "px"');
     background-color: rgba(78, 78, 78, 0.8);
     border-radius: 3px;
 }
