167de9c154f05f060e6a265c91c5e157fa944d4c
rnaisa <rnaisa@users.noreply.github.com>
2023-12-22 09:40:34 +0000

    auto: formatting

--- frontend/src/components/StatisticsComp.test.ts
+++ frontend/src/components/StatisticsComp.test.ts
@@ -3,27 +3,25 @@ import { describe, it, expect, test } from 'vitest';
 import StatisticsComp from './StatisticsComp.vue';
 import { bridge } from '@/global';
 
-
 describe('StatisticsComp.vue', () => {
   it('renders without errors', () => {
-    const wrapper = mount(StatisticsComp)
-    expect(wrapper.exists()).toBe(true)
-  })
+    const wrapper = mount(StatisticsComp);
+    expect(wrapper.exists()).toBe(true);
+  });
 
-  
   it('computes percentage values correctly', async () => {
     // Mock bridge values
     bridge.elevation = 0.5;
     bridge.traffic_noise = 0.6;
     bridge.buildings = 0.7;
-    const wrapper = mount(StatisticsComp)
+    const wrapper = mount(StatisticsComp);
 
     const vm = wrapper.vm as any;
 
     await wrapper.vm.$nextTick();
 
-    expect(vm.stats.elevation.percentageValue).toBe(50)
-    expect(vm.stats.traffic_noise.percentageValue).toBe(60)
-    expect(vm.stats.buildings.percentageValue).toBe(70)
-  })
-})
+    expect(vm.stats.elevation.percentageValue).toBe(50);
+    expect(vm.stats.traffic_noise.percentageValue).toBe(60);
+    expect(vm.stats.buildings.percentageValue).toBe(70);
+  });
+});
